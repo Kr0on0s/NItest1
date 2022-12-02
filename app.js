6137ae3b20d47af5510889ec581b60cbcea703b0
Kr0on0s <armanyigit@hotmail.com>
2022-12-02 04:08:49 +0100

    final5

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname , "htmlfile\\index.html"))
 })
 
+app.get("/images/:file",(req,res)=>{
+    res.sendFile(path.join(__dirname , "images\\"+req.params.file))
+})
 
-app.listen(8000,()=>{
-    console.log("server is running at Port 8000")
+app.get("/style/:file",(req,res)=>{
+    res.sendFile(path.join(__dirname , "style\\"+req.params.file))
+})
+
+app.get("/scripts/:file",(req,res)=>{
+    res.sendFile(path.join(__dirname , "scripts\\"+req.params.file))
+})
+
+var PORT = process.env.PORT? process.env.PORT : 8000;
+
+app.listen(PORT, function(err){
+	if (err) console.log("Error in server setup")
+	console.log("Server listening on Port", PORT);
 })
