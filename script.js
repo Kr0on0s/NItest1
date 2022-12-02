912e9766445775261dd6ad2cb6e245ed1d610d95
Kr0on0s <armanyigit@hotmail.com>
2022-12-02 01:37:23 +0100

    init server node

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -7,3 +12,15 @@ function changeColor(){
     const body = document.querySelector("body")
     body.style.backgroundColor = `rgb(${getRandomArbitrary(0,255)},${getRandomArbitrary(0,255)},${getRandomArbitrary(0,255)})`;
 }   
+
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
