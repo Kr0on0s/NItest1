c3334b10cc4407dbd038ebf595cbbe285bfaf3b2
Kr0on0s <119619829+Kr0on0s@users.noreply.github.com>
2022-12-02 04:44:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-const express = require("express")
-const path = require("path")
-const app = express()
+var express = require('express');
 
+var app = express();
+app.use(express.static("."));
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname , "htmlfile\\index.html"))
-})
-
-app.get("/images/:file",(req,res)=>{
-    res.sendFile(path.join(__dirname , "images\\"+req.params.file))
-})
-
-app.get("/style/:file",(req,res)=>{
-    res.sendFile(path.join(__dirname , "style\\"+req.params.file))
-})
-
-app.get("/scripts/:file",(req,res)=>{
-    res.sendFile(path.join(__dirname , "scripts\\"+req.params.file))
-})
-
-var PORT = process.env.PORT? process.env.PORT : 8000;
-
-app.listen(PORT, function(err){
-	if (err) console.log("Error in server setup")
-	console.log("Server listening on Port", PORT);
-})
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Listening on', port);
+});
